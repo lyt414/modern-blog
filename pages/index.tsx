c082e2bc0b98a791efaaf40917511a81c2562fda
Yitong Liu <yitong414@gmail.com>
2022-02-07 10:11:16 -0500

    Codebase setup

---
+++ pages/index.tsx
@@ -0,0 +1,29 @@
+import Head from 'next/head'
+
+const posts = [
+  {title:'React Testing', excerpt: 'Learn React Testing'},
+  {title:'React Tailwind CSS', excerpt: 'Learn React with Tailwind'},
+];
+
+export default function Home() {
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <Head>
+        <title>Modern Blog</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className ='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        <div className = 'lg:col-span-8 col-span-1'>
+        {posts.map((post, index) => (
+          <div>
+              {post.title}
+              {post.excerpt}
+            </div>
+        ))}
+        </div>
+        <div className = 'lg:col-span-4 col-span-1'>
+        </div>
+      </div>
+    </div>
+  )
+}
