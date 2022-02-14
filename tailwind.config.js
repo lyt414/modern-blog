473b0161ac9387f3d4160cb8f23c039f06695a86
Yitong Liu <yitong414@gmail.com>
2022-02-14 00:03:33 -0500

    Update css config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,12 @@
+const num = [1, 2, 3, 4, 5, 6, 8, 10, 12]
+const whitelist = []
+num.map((x) => {
+  whitelist.push('translate-x-' + x)
+  whitelist.push('-translate-x-' + x)
+  whitelist.push('translate-y-' + x)
+  whitelist.push('-translate-y-' + x)
+})
+
 module.exports = {
   content: [
     "./pages/**/*.jsx",,
@@ -7,5 +16,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: []
+  plugins: [],
+  options: {
+    whitelist,
+  }
 }
